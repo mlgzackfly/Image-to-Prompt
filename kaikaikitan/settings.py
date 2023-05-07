6759ef6bc8a182ff1bb3ad5d6707637d448079cc
mlgzackfly <mlgzackfly@gmail.com>
2023-05-07 19:48:45 +0800

    feat(settings): add media file configuration

--- kaikaikitan/settings.py
+++ kaikaikitan/settings.py
@@ -130,3 +130,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Media files (user uploaded files)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
