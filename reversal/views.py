31e034958908ce4ee963816101e4222810a2541e
mlgzackfly <mlgzackfly@gmail.com>
2023-05-09 14:48:48 +0800

    feat: use base64 response image and change file_url into file

--- reversal/views.py
+++ reversal/views.py
@@ -1,4 +1,5 @@
 # Create your views here.
+import base64
 import time
 import tempfile
 from django.shortcuts import render
@@ -18,12 +19,16 @@ def app(request):
         with tempfile.NamedTemporaryFile(delete=False) as temp_file:
             temp_file.write(upload.read())
         file_url = temp_file.name
+
+        with open(file_url, 'rb') as f:
+            encoded_data = base64.b64encode(f.read()).decode('utf-8')
+
         prompt = ""
         query_image_id = file_url # 圖片位置
         top_k = 20 # 搜尋前幾名
         top_k_image_ids, top_k_similarities, top_k_prompts = similarity.get_similar_images(query_image_id, top_k)
 
-        return render(request, 'reversal/app.html', {'status' : True, 'message' : '上傳成功！','file_url': file_url,'prompt' : top_k_prompts[0],'similarity' : round(top_k_similarities[0],2)*100})
+        return render(request, 'reversal/app.html', {'status' : True, 'message' : '上傳成功！','file': encoded_data,'prompt' : top_k_prompts[0],'similarity' : round(top_k_similarities[0],2)*100})
     return render(request, 'reversal/app.html')
 
 
